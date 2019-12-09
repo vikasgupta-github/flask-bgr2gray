3ed2ec27f3c095ec08be4671befc39512eb6ca10
vishwesh5 <vishweshshrimali5@gmail.com>
2019-12-09 08:11:50 +0530

    Replace cv with PIL

--- app_basic.py
+++ app_basic.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template, request, send_from_directory, send_file
 from flask_bootstrap import Bootstrap
-import cv2
+#import cv2
+from PIL import Image
+
 __author__ = 'ibininja'
 
 app = Flask(__name__)
@@ -30,11 +32,17 @@ def upload():
         file.save(destination)
         absPath = os.path.join(APP_ROOT,destination)
         print(absPath)
-        im = cv2.imread(absPath)
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        # Read image at absPath
+        im = Image.open(absPath)
+        #im = cv2.imread(absPath)
+        # Convert to grayscale
+        gray = im.convert('LA')
+        #gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
         outputFile = "/".join([target, 'out_{}'.format(filename) ])
-        cv2.imwrite(outputFile,gray)
+        # Save grayscale image
+        gray.save(outputFile)
+        #cv2.imwrite(outputFile,gray)
 
     # return send_from_directory("images", filename, as_attachment=True)
     return render_template("complete.html", input=filename , output=outputFile)
